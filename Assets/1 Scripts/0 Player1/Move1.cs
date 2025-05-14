6924fcb2cb4f498f6d9539a412047aac49312efa
ExilonTop <exequielvicencio@gmail.com>
2025-05-14 16:48:39 -0400

    s
    s

--- Assets/1 Scripts/0 Player1/Move1.cs
+++ Assets/1 Scripts/0 Player1/Move1.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Move1 : MonoBehaviour
 {
+    public int player;
     public float velmov;
     public float velrot;
     public GameObject oruga;
@@ -10,21 +11,22 @@ public class Move1 : MonoBehaviour
 
     void Update()
     {
-        Oruga();
         Mira();
+        Oruga();
+        
     }
 
     public void Oruga()
     {
         
-        float y =Input.GetAxis("Vertical")*Time.deltaTime;
+        float y =Input.GetAxis("Vertical"+ player.ToString())*Time.deltaTime;
         Vector3 posicion = transform.position;
-        transform.position=new Vector3(posicion.x, y*velmov+posicion.y, posicion.z);
+        transform.localPosition += transform.up*velmov*y;
         
     }
     private void Mira()
     {
-        float x =Input.GetAxis("Horizontal")*Time.deltaTime;
+        float x =Input.GetAxis("Horizontal"+player.ToString())*Time.deltaTime;
         Quaternion rotation = transform.localRotation;
         rotation.eulerAngles=new Vector3(0,0,-1*x*velrot + rotation.eulerAngles.z);
         transform.localRotation = rotation;
